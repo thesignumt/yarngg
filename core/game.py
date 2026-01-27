39863a1bd10f284fe045c966162bb038e2da0b7c
thesignumt <tcrrxzz@gmail.com>
2026-01-27 21:39:36 +0000

    root game logic

--- core/game.py
+++ core/game.py
@@ -1,1 +1,8 @@
+class Game:
+    def __init__(self):
+        self.running = True
 
+    def run(self):
+        while self.running:
+            print("HAIII YARNGG IS RUNNINNG!! XD")
+            self.running = False
