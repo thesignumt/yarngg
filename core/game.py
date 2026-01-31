12c6caa6e99b8c6fc96fb7980fc485e6038b371e
thesignumt <tcrrxzz@gmail.com>
2026-01-31 13:16:10 +0000

    main game logic

--- core/game.py
+++ core/game.py
@@ -1,8 +1,38 @@
+import pygame
+from . import cfg
+
+from ..utils import pygame_utils as pu
+
+
 class Game:
     def __init__(self):
         self.running = True
 
+        pygame.init()
+        info = pygame.display.Info()
+        w, h = info.current_w, info.current_h
+        self.screen = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
+        self.clock = pygame.time.Clock()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        # game logic here
+        pass
+
+    def draw(self):
+        self.screen.fill(cfg.BLACK)
+
+        pygame.display.flip()
+
     def run(self):
         while self.running:
-            print("HAIII YARNGG IS RUNNINNG!! XD")
-            self.running = False
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(cfg.FPS)
+
+        pygame.quit()
